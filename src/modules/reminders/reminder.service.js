7f715c5670ca048e94c5725d6e65ed5d3e12861c
Lecino Lucas Miranda de Souza <lecinolucas5@gmail.com>
2026-06-23 20:45:24 -0300

    feat: inject structured data into reminder notification metadata

--- src/modules/reminders/reminder.service.js
+++ src/modules/reminders/reminder.service.js
@@ -27,12 +27,21 @@ function getReminderMinutes(setting) {
 }
 
 function buildReminderPayload(appointment) {
+  const timeFormatted = new Intl.DateTimeFormat("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "America/Sao_Paulo",
+  }).format(new Date(appointment.startAt));
+
   return {
     appointmentId: appointment.id,
     clientId: appointment.clientId,
+    clientName: appointment.client?.name,
     professionalId: appointment.professionalId,
+    professionalName: appointment.professional?.name,
     serviceId: appointment.serviceId,
     startAt: appointment.startAt,
+    time: timeFormatted,
   };
 }
 
